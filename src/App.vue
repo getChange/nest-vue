42404703302cfd9f40246755300c7fbac789adee
Han weimin <hanweimin@15w.com>
2023-05-19 18:33:11 +0800

    校验码创建

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,67 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, reactive, ref } from 'vue'
+
+const labelPosition = ref<string>('right')
+
+const codeUrl = ref<string>('/api/user/code')
+
+const resetCode = () => codeUrl.value = codeUrl.value + '?' + Math.random()
+
+const formLabelAlign = reactive({
+  name: '',
+  password: '',
+  code: '',
+})
+
+const submit = async () => {
+  await fetch('/api/user/create', {
+    method: 'POST',
+    body: JSON.stringify(formLabelAlign),
+    headers: {
+      'content-type': 'application/json'
+    }
+  }).then(res => res.json())
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="wraps">
+    <el-form :label-position="labelPosition" label-width="100px" :model="formLabelAlign" style="max-width: 460px">
+      <el-form-item label="账号">
+        <el-input v-model="formLabelAlign.name" />
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input type="password" v-model="formLabelAlign.password" />
+      </el-form-item>
+      <el-form-item label="验证码">
+        <div style="display:flex">
+          <el-input v-model="formLabelAlign.code" />
+          <img @click="resetCode" :src="codeUrl" alt="">
+        </div>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="submit">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+* {
+  padding: 0;
+  margin: 0;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.wraps {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: inherit;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+html,
+body,
+#app {
+  height: 100%;
 }
 </style>
